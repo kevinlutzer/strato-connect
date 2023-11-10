70b5f75b2a02defc9154c8823ae2a35940439173
Kevin Lutzer <kevinlutzer@proton.me>
2023-11-10 01:08:03 -0500

    add the rp2040/si4663 test pcb

--- code/strato-connect/strato-connect.ino
+++ code/strato-connect/strato-connect.ino
@@ -1,8 +1,9 @@
 #include "rf4463.hpp"
-#include "ax25.hpp"
-#include "i2ccontroller.hpp"
-#include <SPI.h>
-#include <Wire.h>
+// #include "ax25.hpp"
+// #include "i2ccontroller.hpp"
+// #include <SPI.h>
+// #include <Wire.h>
+// #include <Adafruit_BME280.h>
 
 #define IRQ 2
 #define SDN 9
@@ -13,12 +14,10 @@
 #define LED_TX 17
 #define INT 15
 
-AX25 ax25 = AX25();
 RF4463 rf4463 = RF4463(IRQ, SDN, SEL);
-I2CController i2ccontroller = I2CController(&ax25, &Wire);
-unsigned char tx_buf[]={"NEW HELLO WORLD!!!"};
+unsigned char tx_buf[] = { "NEW HELLO WORLD!!!" };
 unsigned char val;
-unsigned char flag=0;    //  flag of rx mode
+unsigned char flag = 0;  //  flag of rx mode
 unsigned char rx_len;
 unsigned char rx_buf[20];
 
@@ -30,24 +29,22 @@ void setup() {
   pinMode(INT, OUTPUT);
 
   digitalWrite(LED_RX, HIGH);
-  digitalWrite(LED_RX, HIGH);
+
   digitalWrite(INT, HIGH);
 
-  while(true) {
-    Serial.println("Waiting for serial...");
-    delay(1000);
-  }
-
-  if(!rf4463.init())
+  if (!rf4463.init())
     Serial.println("Init fail!");
   else
     Serial.println("Init success!");
 
-  if(!rf4463.setTxPower(127))
+  if (!rf4463.setTxPower(127))
     Serial.println("Failed to set power to max!");
 }
 
 void loop() {
-  rf4463.txPacket(tx_buf,sizeof(tx_buf));
+  digitalWrite(LED_TX, LOW);
+  delay(1000);
+  digitalWrite(LED_TX, HIGH);
+  rf4463.txPacket(tx_buf, sizeof(tx_buf));
   delay(1000);
 }
